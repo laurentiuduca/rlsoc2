db6f534665cd5c60a95075db9b9f6382491d258c
Laurentiu-Cristian Duca <laurentiu.duca@gmail.com>
2024-05-16 16:33:04 +0300

    dbg

--- rvcorem.v
+++ rvcorem.v
@@ -757,8 +757,8 @@ module m_RVCoreM(CLK, RST_X, w_stall, w_hart_id, w_ipi, r_halt, w_insn_addr, w_d
         if(/*pc >= 32'hc00276b8 && pc <= 32'hc00276d4*/
             pc == 32'hc00276bc && w_hart_id == 0 /*&& laurpc != pc*/ && regs.mem[1] == 32'hc002b5dc
             && w_mtime >= 170000000) begin
-            $display("test_bit div pc=%x a0=%x a1=%x a5=%x grant=%x state=%x", 
-                pc, regs.mem[10], regs.mem[11], regs.mem[15], w_grant, state);
+            $display("test_bit div pc=%x a0=%x a1=%x a5=%x grant=%x w_tlb_req=%x w_busy=%x w_ex1_busy=%x state=%x", 
+                pc, regs.mem[10], regs.mem[11], regs.mem[15], w_grant, w_tlb_req, w_busy, w_ex1_busy, state);
             laurpc <= pc;
         end
 
@@ -966,7 +966,7 @@ module m_RVCoreM(CLK, RST_X, w_stall, w_hart_id, w_ipi, r_halt, w_insn_addr, w_d
 
     assign w_state = (!RST_X | r_halt)                                         ? 0      : // `S_INI
                      (w_com)                                                   ? `S_COM :
-                     (w_busy || w_ex1_busy)                                    ? state  :
+                     ((w_busy/* && !w_ex1*/) || w_ex1_busy)                    ? state  :
                      (state==`S_FIN)                                           ? `S_INI :
                      (state==`S_IF  && w_nalign4 && r_if_state!=3)             ? `S_IF  : // Note
                      (state==`S_EX1 && !r_op_AMO && !r_op_LOAD && !r_op_STORE) ? `S_WB  :
